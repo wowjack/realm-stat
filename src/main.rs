c447f6e92e9a0e6e11fa629eb01b0b08b293bcf0
Jack Kingham <wowknead@gmail.com>
2023-07-08 15:43:34 -0400

    Basic packet capture.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
+use std::io::Write;
+
+
 fn main() {
     let devices = pcap::Device::list().expect("device list failed");
-    let device = devices.iter().filter_map(|d| if d.desc == Some("802.11ac Wireless LAN Card".to_string()) {Some(d)} else {None}).next().expect("No device found").clone();
+    for (ind, d) in devices.iter().enumerate() {
+        println!("{ind}: {}", d.clone().desc.unwrap_or("Error".to_string()));
+    }
+    let mut selected = std::usize::MAX;
+    while selected >= devices.len() {
+        let mut input = String::new();
+        print!("Select Network Adapter: ");
+        let _ = std::io::stdout().flush();
+        let _ = std::io::stdin().read_line(&mut input);
+        if let Ok(ind) = input.trim_end().parse::<usize>() {
+            selected = ind;
+        }
+    }
+    
+    let device = devices[selected as usize].clone();
+
+    println!("Listening with addr {:?}", device.clone().addresses[0].addr);
     
 
     // Setup Capture
-    let mut cap = pcap::Capture::from_device(device)
+    let mut cap = pcap::Capture::from_device(device.clone())
         .unwrap()
         .immediate_mode(true)
         .open()
         .unwrap();
 
-    // get a packet and print its bytes
-    println!("{:?}", cap.next_packet());
-
+    cap.filter(&format!("dst host {}", device.addresses[0].addr.to_string()), false).expect("Error with packet filter");
+    
+    
+    for _ in 0..10 {
+        let slice = etherparse::SlicedPacket::from_ethernet(cap.next_packet().unwrap().data).expect("Error reading packet data");
+            match slice.ip.expect("Failed to get IP") {
+            etherparse::InternetSlice::Ipv4(header, _ext) => {
+                println!("{:?} to {:?}", header.source_addr(), header.destination_addr());
+            },
+            _ => panic!("Not an ipv4 packet")
+        }
+    }
+    
 }
